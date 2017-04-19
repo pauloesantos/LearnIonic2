cff11ee6072f7ef73218992fdc8b7110b330e24a
Paulo Eduardo Fagundes dos Santos <paulo.esantos@gmail.com>
2017-04-19 14:41:37 -0300

    Inciar Quotes app

--- QuotesApp/src/pages/home/home.ts
+++ QuotesApp/src/pages/home/home.ts
@@ -6,16 +6,9 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-  
-  public tapCounter: number = 0;
+
   constructor(public navCtrl: NavController) {
 
-  }
-  buttonTapped() {
-    this.tapCounter++;
-  }
-  reset() {
-    this.tapCounter = 0;
   }
 
 }
